8ba8a121c488bb2891565e942e8c3949889bb609
Tristan McKinney <natsirguy@gmail.com>
2017-02-12 23:47:42 -0800

    Add insertion and erasing functions and keybindings.

--- flykey.el
+++ flykey.el
@@ -47,6 +47,13 @@
   (concat flykey-dir "/flykey.sh")
   "The shell file used to create keybindings.")
 
+;; These three variables will correspond to the buffer-local names of the
+;; original buffer from which FlyKey is run, the buffer with the keybindings,
+;; and the buffer from which text is inserted.
+(defvar flykey-oldbuf)
+(defvar flykey-flybuf)
+(defvar flykey-insertbuf)
+
 (defun flykey-read-quoted-cmds (comquotes)
   "Read COMQUOTES, a list of quoted commands, and return a list of commands."
   (if comquotes (cons (car (read-from-string (car comquotes)))
@@ -98,38 +105,69 @@
 	  (insert (concat "#" (format "%s" oldmajor))))
 	flybuf))))
 
-(defun flykey-open-windows (flybuf bufinsert)
-  "Split the current window, then split one of the halves and display FLYBUF and BUFINSERT there."
-  (set-window-buffer (split-window-below) bufinsert)
-  (with-selected-window (get-buffer-window bufinsert)
+(defun flykey-open-windows (flybuf insertbuf)
+  "Split the current window, then split one of the halves and display FLYBUF and INSERTBUF there."
+  (set-window-buffer (split-window-below) insertbuf)
+  (with-selected-window (get-buffer-window insertbuf)
     (set-window-buffer (split-window-below) flybuf)))
 
-(defun flykey-set-up-buffers (flybuf bufinsert)
-  "Open the windows for FLYBUF and BUFINSERT and apply the keybindings."
+(defun flykey-set-up-buffers (flybuf insertbuf)
+  "Open the windows for FLYBUF and INSERTBUF and apply the keybindings."
   (let ((pmap (copy-keymap (current-local-map))))
-    (with-current-buffer bufinsert
+    (with-current-buffer insertbuf
       (use-local-map pmap))
-    (flykey-open-windows flybuf bufinsert)
-    (flykey-add-bindings flybuf bufinsert)))
+    (flykey-open-windows flybuf insertbuf)
+    (flykey-add-bindings flybuf insertbuf)
+    ;; Add some keybindins to insert insertbuf or erase it.
+    (with-current-buffer insertbuf
+      (local-set-key (kbd "C-c i") 'flykey-insert-and-close)
+      (local-set-key (kbd "C-c c") 'flykey-clear-insertbuf))))
+
+(defun flykey-set-local-vars (buf oldbuf flybuf insertbuf)
+  "In BUF, set local variables OLDBUF, FLYBUF, and INSERTBUF."
+  (with-current-buffer buf
+    (make-local-variable 'flykey-oldbuf)
+    (setq flykey-oldbuf oldbuf)
+    (make-local-variable 'flykey-flybuf)
+    (setq flykey-flybuf flybuf)
+    (make-local-variable 'flykey-insertbuf)
+    (setq flykey-insertbuf insertbuf)))
 
 (defun flykey-run ()
   "Run flykey."
   (interactive)
   (let ((oldbuf (current-buffer))
 	(flybuf (flykey-open-flyk))
-	(bufinsert (generate-new-buffer "*flykey*")))
-    (flykey-set-up-buffers flybuf bufinsert)
-    (select-window (get-buffer-window bufinsert))
-    ;; (make-local-variable 'flykey-fkold)
-    ;; (defconst flykey-fkold oldbuf
-    ;;   "The buffer from which flykey was called. Buffer-local.")
-    ;; (make-local-variable 'flykey-fkfile)
-    ;; (defconst flykey-fkfile (buffer-file-name flyfilebuf)
-    ;;   "The flykey file name. Buffer-local.")
-    ;; (make-local-variable 'flykey-fkpmap)
-    ;; (defconst flykey-fkpmap pmap
-    ;;   "The parent keymap that is being inherited. Buffer-local.")
-    ))
+	(insertbuf
+	 (get-buffer-create
+	  (concat "*flykey-" (buffer-name (current-buffer)) "*"))))
+    (flykey-set-up-buffers flybuf insertbuf)
+    (select-window (get-buffer-window insertbuf))
+    (flykey-set-local-vars oldbuf oldbuf flybuf insertbuf)
+    (flykey-set-local-vars flybuf oldbuf flybuf insertbuf)
+    (flykey-set-local-vars insertbuf oldbuf flybuf insertbuf)))
+
+(defun flykey-close-windows ()
+  "Close the windows associated with flybuf and insertbuf."
+  (select-window (get-buffer-window flykey-oldbuf))
+  (delete-window (get-buffer-window flykey-flybuf))
+  (delete-window (get-buffer-window flykey-insertbuf)))
+
+(defun flykey-insert-insertbuf ()
+  "Insert the contents of insertbuf into oldbuf."
+  (flykey-insert-buffer flykey-insertbuf flykey-oldbuf))
+
+(defun flykey-insert-and-close ()
+  "Insert insertbuf and close windows."
+  (interactive)
+  (flykey-insert-insertbuf)
+  (flykey-close-windows))
+
+(defun flykey-clear-insertbuf ()
+  "Erase the contents of insertbuf."
+  (interactive)
+  (with-current-buffer flykey-insertbuf
+    (erase-buffer)))
 
 (provide 'flykey)
 ;;; flykey.el ends here
